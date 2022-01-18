279a180bb6d40c0218aed5b06c1cd506a035a4a7
binder@knowledgeagent.de <jbinder@laptopka22.knowledgeagent.local>
2022-01-18 11:58:21 +0100

    increase excerpt max length

--- src/extractors/generic/excerpt/extractor.js
+++ src/extractors/generic/excerpt/extractor.js
@@ -4,7 +4,7 @@ import { extractFromMeta, stripTags } from 'utils/dom';
 
 import { EXCERPT_META_SELECTORS } from './constants';
 
-export function clean(content, $, maxLength = 200) {
+export function clean(content, $, maxLength = 500) {
   content = content.replace(/[\s\n]+/g, ' ').trim();
   return ellipsize(content, maxLength, { ellipse: '&hellip;' });
 }
@@ -13,10 +13,10 @@ const GenericExcerptExtractor = {
   extract({ $, content, metaCache }) {
     const excerpt = extractFromMeta($, EXCERPT_META_SELECTORS, metaCache);
     if (excerpt) {
-      return clean(stripTags(excerpt, $));
+      return clean(stripTags(excerpt, $), $, 10000);
     }
     // Fall back to excerpting from the extracted content
-    const maxLength = 200;
+    const maxLength = 500;
     const shortContent = content.slice(0, maxLength * 5);
     return clean($(shortContent).text(), $, maxLength);
   },
