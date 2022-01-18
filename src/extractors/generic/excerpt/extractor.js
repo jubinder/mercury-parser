7fdf13283d309a37881809388db9964b4c72736d
binder@knowledgeagent.de <jbinder@laptopka22.knowledgeagent.local>
2022-01-18 16:11:07 +0100

    first paragraph fallback for excerpts

--- src/extractors/generic/excerpt/extractor.js
+++ src/extractors/generic/excerpt/extractor.js
@@ -15,6 +15,26 @@ const GenericExcerptExtractor = {
     if (excerpt) {
       return clean(stripTags(excerpt, $), $, 10000);
     }
+	
+	// First paragraph fallback
+	var $content = $('<div>'+content+'</div>')
+	var $pElements = $content.find('p');
+	var $pElementsWithText = $pElements.filter(function(){
+		return $(this).text().trim().length > 30;
+	})
+	if($pElementsWithText.length > 1){
+		return $pElementsWithText.first().text();
+	}
+	/*
+	// First div fallback
+	var $divElements = $content.find('div');
+	var $divElementsWithText = $divElements.filter(function(){
+		return $(this).text().trim().length > 30;
+	})
+	if($divElementsWithText.length > 1){
+		return $divElementsWithText.first().text();
+	}	
+	*/
     // Fall back to excerpting from the extracted content
     const maxLength = 500;
     const shortContent = content.slice(0, maxLength * 5);
