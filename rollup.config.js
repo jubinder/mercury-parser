a134a1252128c97d4c691554c32abdfc73e9f157
binder@knowledgeagent.de <jbinder@laptopka22.knowledgeagent.local>
2022-01-18 11:58:35 +0100

    tweak rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'src/mercury.js',
   plugins: [
     commonjs(),
     babel({
-      externalHelpers: false,
-      runtimeHelpers: true,
+      babelHelpers: 'runtime'
     }),
   ],
   treeshake: true,
